206c38625baf32568158ec30027580e477b73c89
Vichenchov <max.viva10@gmail.com>
2020-11-24 21:20:30 +0200

    added Connection page

--- JS/Connection.js
+++ JS/Connection.js
@@ -1,1 +1,32 @@
-alert("working");
+var imgNum = document.querySelectorAll("img").length;
+
+for (var i = 0; i < imgNum; i++) {
+  document.querySelectorAll("img")[i].classList.add("hide");
+}
+
+var y = 0;
+if ($(window).width() > 360) {
+  setInterval(function show() {
+    document.querySelectorAll("img")[y].classList.remove("hide");
+    y++;
+    if (y === 7) {
+      clearInterval(show);
+      window.location.replace("BirthdayBlessing.html");
+    }
+  }, 400);
+}
+
+y = 0;
+if ($(window).width() <= 360) {
+  setInterval(function show() {
+    if (y > 0) {
+      document.querySelectorAll("img")[y - 1].classList.add("hide");
+    }
+    document.querySelectorAll("img")[y].classList.remove("hide");
+    y++;
+    if (y === 7) {
+      clearInterval(show);
+      window.location.replace("BirthdayBlessing.html");
+    }
+  }, 400);
+}
