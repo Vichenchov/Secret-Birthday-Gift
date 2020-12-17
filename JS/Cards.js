8d25699e73469894578391cf378ba8fc96a92d89
Vichenchov <max.viva10@gmail.com>
2020-12-17 16:20:23 +0200

    started with the conntent

---
+++ JS/Cards.js
@@ -0,0 +1,18 @@
+// var vid = document.getElementById("vid"); 
+
+
+
+// function pauseVid() { 
+//   vid.pause(); 
+// } 
+
+// $(".btn").click(function() {
+//     var chosenButton = $(this).attr("id");
+//     var vid = "vid-" + chosenButton;
+//     console.log(vid);
+//     $("#" + vid).play();
+// });
+
+$(".btn-2").onclick = function(){
+    $("#vid-2").play();
+}
