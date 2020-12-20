d6d028ff8b6d7aa2d63e18c020774459a4d43065
Vichenchov <max.viva10@gmail.com>
2020-12-20 23:21:39 +0200

    done with the cards page - added audio

--- JS/Cards.js
+++ JS/Cards.js
@@ -12,4 +12,32 @@ function playVid2() {
 
 video.addEventListener("mouseover", function () {
     this.play();
-}); 
+});
+
+
+$(".btn").click(function () {
+    var userClickedButton = $(this).attr("id");
+    playSound(userClickedButton);
+});
+
+function playSound(btn) {
+    var audio = new Audio();
+    switch (btn) {
+        case "btn-1":
+            audio = new Audio("Audio/Iloveyou.mp3");
+            audio.play();
+            break;
+        case "btn-3":
+            audio = new Audio("Audio/Shovala.mp3");
+            audio.play();
+            break;
+        case "btn-4":
+            audio = new Audio("Audio/RoziNaknik.mp3");
+            audio.play();
+            break;
+        case "btn-6":
+            audio = new Audio("Audio/Red.mp3");
+            audio.play();
+            break;
+    }
+}
